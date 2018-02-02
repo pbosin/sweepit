05058923b00037cf2c0425ba098e4f45cc9f054e
Pavel Bosin <pavel@bosin.net>
2018-02-01 17:53:00 -0800

    adding tests

--- tests/helpers/gameHelper.test.js
+++ tests/helpers/gameHelper.test.js
@@ -2,12 +2,42 @@ import React from 'react';
 import { expect } from 'chai';
 
 // Helper
-import gameHelper from '../../src/js/helpers/gameHelper';
+import * as gameHelper from '../../src/js/helpers/gameHelper';
 
 describe('gameHelper Helper', () => {
 
   it('Waiting for Testing', () => {
     return true;
+  });
+
+  describe('reveal all mines', () => {
+    let rows = [];
+    beforeEach(() => {
+      // [_,x]
+      // [x,_]
+      rows = [
+          [
+              {masked: true, count: 0, mine: false, marked: false},
+              {masked: true, count: 0, mine: true, marked: false}
+          ],[
+              {masked: true, count: 0, mine: true, marked: false},
+              {masked: true, count: 0, mine: false, marked: false}
+          ],
+      ];
+    });
+
+    it('should mark all mines after game is won', () => {
+      let revealedRows = gameHelper.revealAllMines(rows);
+      expect(revealedRows[0][0].marked).to.equal(false);
+      expect(revealedRows[0][1].marked).to.equal(true);
+      expect(revealedRows[1][0].marked).to.equal(true);
+      expect(revealedRows[1][1].marked).to.equal(false);
+    });
+
+    it('should find masked empty cells', () => {
+      expect(gameHelper.haveNonMineMasked(rows)).to.be.ok;
+    });
+
   });
 
 });
